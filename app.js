6e4d9e6847eae0fb5ab37c4198388e55c1bedbdd
Steven Abrams <steven.abrams86@gmail.com>
2015-08-07 16:42:19 -0600

    added twitter

--- app.js
+++ app.js
@@ -4,15 +4,26 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var Twitter = require('twitter');
+
+require('dotenv').load();
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -24,6 +35,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+client.get('search/tweets', {q: 'cats', geocode: '39.7392%2C%20104.9903%2C%2010'
+}, function(error, tweets, response){
+  // ?max_id=629778869537644543&q=cats&include_entities=2
+  //  console.log(tweets);
+   console.log(response.headers['content-length']);
+});
+
+client.get('search/tweets', {q: 'cats',
+}, function(error, tweets, response){
+  // ?max_id=629778869537644543&q=cats&include_entities=2
+  //  console.log(tweets);
+   console.log(response.headers['content-length']);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
