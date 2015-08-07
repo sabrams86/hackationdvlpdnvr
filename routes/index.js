a5ffeb2e964829ec417e1ea2f0f85c40cd0db7d1
Roberto Ortega <berto.ort@gmail.com>
2015-08-07 15:57:51 -0600

    reddit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var unirest = require('unirest');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var title = []
+  unirest.get('https://www.reddit.com/subreddits.json')
+  .end(function (response) {
+    var body = JSON.parse(response.raw_body);
+    var title = []
+    body.data.children.forEach(function (subreddits) {
+      title.push(subreddits.data.url)
+    })
+    title.forEach(function (subreddit) {
+
+    })
+    unirest.get('https://www.reddit.com'+ title[0].toString()  + '.json')
+    .end(function (response) {
+      var titles = []
+      databody = JSON.parse(response.raw_body)
+      databody.data.children.forEach(function (tops) {
+        titles.push(tops.data.title)
+      })
+      console.log(titles);
+      res.render('index', { title: 'Express' });
+    })
+  });
 });
+
+var titlecall = function (title) {
+
+}
 
 module.exports = router;
